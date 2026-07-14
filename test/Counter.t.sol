54a87c311b8d2e60d8a92ceaa4dc4f195a74c3cc
theoam4 <theoam4@outlook.com>
2026-07-13 22:55:22 -0300

    test: ajusta valor inicial para 1 e asserções nos testes do contrato Counter

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -9,12 +9,17 @@ contract CounterTest is Test {
 
     function setUp() public {
         counter = new Counter();
-        counter.setNumber(0);
+        counter.setNumber(1);
     }
 
     function test_Increment() public {
         counter.increment();
-        assertEq(counter.number(), 1);
+        assertEq(counter.number(), 2);
+    }
+
+    function test_Decrement() public {
+        counter.decrement();
+        assertEq(counter.number(), 0);
     }
 
     function testFuzz_SetNumber(uint256 x) public {
